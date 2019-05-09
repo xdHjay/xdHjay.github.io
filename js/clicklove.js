8fa0a09a2b663dd704e005c6ca848c86bcd98b54
Jie <hosjay@gmail.com>
2019-05-09 17:43:47 +0800

    Site updated: 2019-05-09 17:43:41

--- js/clicklove.js
+++ js/clicklove.js
@@ -1,27 +1,38 @@
+
 var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
-        var a = new Array("python", "java", "SQL", "C", "C++", "php", "可靠" ,"低价", "高效");
-        var $i = $("<span />").text(a[a_idx]);
+        var a = new Array
+        ("python", "java", "SQL", "C", "C++", "php", "可靠" ,"低价", "高效");
+        var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
         y = e.pageY;
         $i.css({
-            "z-index": 999999999999999999999999999999999999999999999999999999999999999999999,
+            "z-index": 5,
             "top": y - 20,
             "left": x,
             "position": "absolute",
             "font-weight": "bold",
-            "color": "#ff6651"
+            "color": s()
         });
         $("body").append($i);
         $i.animate({
             "top": y - 180,
             "opacity": 0
         },
-        1500,
+        3000,
         function() {
-            $i.remove();
+        $i.remove();
         });
     });
+    setTimeout('delay()', 2000);
 });
+
+function delay() {
+    $(".buryit").removeAttr("onclick");
+}
+
+function s() {
+    return "rgb(" + ~~ (255 * Math.random()) + "," + ~~ (255 * Math.random()) + "," + ~~ (255 * Math.random()) + ")"
+}
